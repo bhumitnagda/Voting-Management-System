70b641c7fba5f0aecc92f0e8ec70116f0440a867
bhumitnagda <158726156+bhumitnagda@users.noreply.github.com>
2024-09-02 07:13:18 +0530

    Add files via upload

---
+++ db/Voting.sql
@@ -0,0 +1,13 @@
+Create Database Voting;
+Use Voting;
+
+select * from student;
+desc student;
+-- truncate student ;
+
+-- delete from student where id='2';
+
+use voting;
+select * from votes;
+-- truncate votes;
+
